e5b0e60890ddabeb9dc89eccc4a2a7544c250f15
Nacho <icofre@gmail.com>
2012-08-07 16:41:01 +0200

    movil

--- templates/main.php
+++ templates/main.php
@@ -23,6 +23,11 @@
 	<div class="navbar navbar-fixed-top">
 		<div class="navbar-inner">
 			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
 				<a class="brand" href="./">{WE [&hearts;] JS}</a>
 				<div class="nav-collapse">
 					<ul class="nav">
