5862ac69e83bc2ace8924ac29e5751bfb073226e
tomas.corral <7e16f4105658d7d343b2c6000f03d8b7>
2013-01-15 12:50:13 +0100

    Update speakers info

--- templates/welovejs4-2013-winter.php
+++ templates/welovejs4-2013-winter.php
@@ -22,21 +22,24 @@
                     <div class="span2">
                         <div class="thumbnail"><img width="200" height="200" src="<?=$assets_path?>img/speakers/pablo.bouzada.jpg"
                                                     alt="Pablo Bouzada">
-							<ul class="unstyled inline">
+							<!--<ul class="unstyled inline">
                             	<li><a href="mailto:pbousan@gmail.com" class="sb small email">e-mail</a></li>
                             	<li><a href="https://twitter.com/#!/pbousan" class="sb small twitter">Twitter</a></li>
-                            	<!--<li><a href="https://www.facebook.com/carlos.villu" class="sb small facebook">Facebook</a></li>
-                            	<li><a href="http://es.linkedin
-						.com/pub/carlos-villuendas/22/441/449" class="sb small linkedin">Linkedin</a></li>-->
-                        	</ul>
+                        	</ul>-->
 						</div>
                     </div>
                     <div class="span10">
                         <h3>Charla: Introducci&oacute;n a TypeScript</h3>
                         <p>Por <strong>Pablo Bouzada</strong></p>
                         <p>
+							<div><strong>BIO:</strong></div>
                             Especialista en tecnolog&iacute;as .NET en Pasiona Consulting, con 10 a&ntilde;os de experiencia en el mundo del desarrollo de software. Ha trabajado tanto para peque&ntilde;as empresas como para grandes corporaciones, y en todas ha encontrado errores comunes, por eso le encantan los antipatrones. Pragm&acute;tico y agilista convencido, est&acute; siempre buscando la forma de hacer mejor las cosas, y para ello sigue las sabias palabras del maestro Yoda: &quote;Hazlo o no lo hagas, pero no lo intentes.&quote; Miembro de la Fundaci&oacute;n [T]echdencias, colaborador de CatDotNet, Agile Barcelona y Programandonet.com
                         </p>
+						<p>
+							<div><strong>Sobre la charla:</strong></div>
+                        	Cada vez se desarrollan con JavaScript aplicaciones m&aacute;s grandes y complejas, y por tanto m&aacute;s dif&iacute;ciles de mantener. Para ayudarnos en esta ardua tarea nace TypeScript.
+                        TypeScript es fruto del trabajo de Anders Hejlsberg, uno de los creadores de C# y pretende ayudar a los equipos de programaci&oacute;n a definir interfaces entre componentes de software y reducir los conflictos de nomenclatura mediante la organizaci&oacute;n del c&oacute;digo en m&oacute;dulos que se pueden cargar de forma din&aacute;mica. Tambi&eacute;n a&ntilde;ade un sistema de tipado opcional que se comprueba en tiempo de compilaci&oacute;n, lo que elimina las comprobaciones de tipo en nuestro c&oacute;digo.
+						</p>
                     </div>
                 </div>
                 <hr/>
@@ -44,13 +47,10 @@
                     <div class="span2">
                         <div class="thumbnail"><img width="200" height="200" src="<?=$assets_path?>img/speakers/tbd.jpg"
                                                     alt="TBD">
-                            <ul class="unstyled inline">
+                            <!--<ul class="unstyled inline">
                                 <li><a href="mailto:" class="sb small email">e-mail</a></li>
                                 <li><a href="https://twitter.com/#!/" class="sb small twitter">Twitter</a></li>
-                                <!--<li><a href="https://www.facebook.com/carlos.villu" class="sb small facebook">Facebook</a></li>
-<li><a href="http://es.linkedin
-.com/pub/carlos-villuendas/22/441/449" class="sb small linkedin">Linkedin</a></li>-->
-                            </ul>
+                            </ul>-->
 						</div>
                     </div>
                     <div class="span10">
@@ -64,25 +64,30 @@
                     <div class="span2">
                         <div class="thumbnail"><img width="200" height="200" src="<?=$assets_path?>img/speakers/fernando.escolar.jpg"
                                                     alt="Fernando Escolar">
-                            <ul class="unstyled inline">
+                            <!--<ul class="unstyled inline">
                                 <li><a href="mailto:fer.escolar@gmail.com" class="sb small email">e-mail</a></li>
                                 <li><a href="https://twitter.com/#!/fernandoescolar" class="sb small twitter">Twitter</a></li>
-                                <!--<li><a href="https://www.facebook.com/carlos.villu" class="sb small facebook">Facebook</a></li>
-<li><a href="http://es.linkedin
-.com/pub/carlos-villuendas/22/441/449" class="sb small linkedin">Linkedin</a></li>-->
-                            </ul>
+                            </ul>-->
 						</div>
                     </div>
                     <div class="span10">
                         <h3>Workshop: TypeScript - Con las manos en la masa</h3>
                         <p>Por <strong>Fernando Escolar</strong></p>
-                        <p><a href=""
+                        <p><a href="https://docs.google.com/document/d/1yayykL_RLlcbcwfryhbBolu9H7uCX7JHbbrMCJ9Fnqw/edit"
                               target="_blank"
                               class="label label-important">Requerimientos</a></p>
                         <p>
+							<div><strong>BIO:</strong></div>
                             Desarrollador especialista en tecnolog&iacute;as .Net en Pasiona Consulting. Convencido de que hay una forma mejor de hacer las cosas: el agilismo ayuda a enriquecer los procesos y es necesario un constante estado de aprendizaje. Divulgador tecnológico en la Fundaci&oacute;n [T]echdencias y colaborador de los grupos de usuarios CatDotNet, Agile Barcelona y programandonet.com.
                         </p>
-                        <p></p>
+                        <p>
+                        	<div>
+								<strong>Sobre el workshop:</strong>
+							</div>
+                        	En este workshop vamos a crear el juego definitivo que va a arrasar en todas las plataformas: "TypeHero", para ello utilizaremos Node.js, Typescript y una buena dosis de patrones de dise&ntilde;o y buenas pr&aacute;cticas como SOLID, KISS o YAGNI.
+                        Iremos paso a paso desarrollando tanto la parte del servidor (en Node.js) como la de cliente (Single Page App) utilizando Typescript en ambos casos.
+                        Si te interesa s&oacute;lo tienes que <strong>traer instalado Node y tu IDE favorito</strong>, lo dem&aacute;s lo ponemos nosotros ;)
+                        </p>
                     </div>
                 </div>
                 <hr/>
@@ -90,13 +95,10 @@
                     <div class="span2">
                         <div class="thumbnail"><img width="200" height="200" src="<?=$assets_path?>img/speakers/tbd.jpg"
                                                     alt="TBD">
-                            <ul class="unstyled inline">
+                            <!--<ul class="unstyled inline">
                                 <li><a href="mailto:" class="sb small email">e-mail</a></li>
                                 <li><a href="https://twitter.com/#!/" class="sb small twitter">Twitter</a></li>
-                                <!--<li><a href="https://www.facebook.com/carlos.villu" class="sb small facebook">Facebook</a></li>
-<li><a href="http://es.linkedin
-.com/pub/carlos-villuendas/22/441/449" class="sb small linkedin">Linkedin</a></li>-->
-                            </ul>
+                            </ul>-->
 						</div>
                     </div>
                     <div class="span10">
