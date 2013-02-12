975f74a2e54e93f0ae50cea59e932c4d3b54e194
natxet <icofre@gmail.com>
2013-02-12 16:06:15 +0100

    fechas correctas para el welovejs4

--- templates/welovejs4-2013-winter.php
+++ templates/welovejs4-2013-winter.php
@@ -3,9 +3,9 @@
 
 
 <div class="page-header">
-	<h1>Pr&oacute;ximo evento: 26 ene 2012
+	<h1>Pr&oacute;ximo evento: 2 de marzo de 2013
 		<a class="btn btn-large btn-primary disabled"
-		   target="_blank">Lunes 14 ene 2012 entradas a la venta</a></h1>
+		   target="_blank">En breve, entradas a la venta</a></h1>
 </div>
 <div class="row">
 	<div class="span12">
