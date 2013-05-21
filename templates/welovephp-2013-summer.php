e68eb8133defcbb1f6cd2b817f31fdc809d2f746
unknown <thomas.klein@ST-L213-THOMKLE.domino.softonic.com>
2013-05-21 18:56:56 +0200

    Initital commit for the next edition of welovejs summer 2013

---
+++ templates/welovephp-2013-summer.php
@@ -0,0 +1,195 @@
+<div class="page-header">
+	<h1>Pr&oacute;ximo evento: 15 de junio de 2013
+		<!--<a class="btn btn-large btn-success" href="http://welovejs5.eventbrite.com/"
+		   target="_blank">¡Consigue tu entrada!</a>-->
+        </h1>
+</div>
+<div class="row">
+	<div class="span12">
+		<nav>
+			<ul class="nav nav-tabs" id="eventContents">
+				<li><a href="#talks">Charlas</a></li>
+				<li><a href="#horario">Horario</a></li>
+			</ul>
+		</nav>
+
+		<div class="tab-content">
+            <div class="tab-pane" id="talks">
+            	<h2>Charlas</h2>
+				<div class="row">
+                    <div class="span2">
+                        <div class="thumbnail">
+                        	<img src="<?=$assets_path?>img/speakers/jose.deblas.jpg" alt="Jose de Blas" />
+                            <ul class="unstyled inline">
+                                <li><a href="https://twitter.com/#!/wecanbuygoogle" class="sb small twitter">Twitter</a></li>
+                            </ul>
+						</div>
+                    </div>
+                    <div class="span10">
+                        <h3>Charla: SEO for Single Page Webapps / Websites</h3>
+                        <p>Por <strong>Jose de Blas</strong></p>
+						<h4>BIO</h4>
+						<p>	Especialista SEO en Softonic con más de 10 años de experiencia en el sector internet.
+							Ha trabajado como programador, CTO y analista web, pero su pasión la encontramos
+							en el posicionamiento web. Es un convencido de que el SEO OnPage no está reñido
+							ni con las buenas prácticas, ni con las nuevas tecnologías. Por ello busca siempre
+							el perfeccionamiento desde un punto de vista técnico.
+						</p>
+						<h4>Sobre la charla</h4>
+                        <p>Hoy en día es difícil encontrarse con webs que no utilicen JS, pero demasiado
+							a menudo las tecnologías más avanzadas chocan con la visibilidad en buscadores.
+							Tendremos la oportunidad de hablar sobre:
+							<ul>
+								<li>¿Qué código pueden seguir los robots?</li>
+								<li>Cómo utilizar javascript en beneficio SEO</li>
+								<li>Cómo hacer visible aplicaciones web single page o webs basadas en AJAX.</li>
+							</ul>
+						 </p>
+                    </div>
+                </div>
+                <hr/>
+
+                <div class="row">
+                    <div class="span2">
+                        <div class="thumbnail">
+                        	<img src="<?=$assets_path?>img/speakers/valentin.gutierrez.png" alt="Valentín Gutierrez">
+                            <ul class="unstyled inline">
+                                <li><a href="https://twitter.com/#!/vnico" class="sb small twitter">Twitter</a></li>
+                            </ul>
+						</div>
+                    </div>
+                    <div class="span10">
+                        <h3>Charla: Seguridad en JS y especialmente en Node.js</h3>
+                        <p>Por <strong>Valentín Gutierrez</strong></p>
+                        <h4>BIO</h4>
+                        <p>Web Application Security Specialist in Softonic.</p>
+                        <h4>Sobre la charla</h4>
+                        <p>t.b.a.</p>
+                    </div>
+                </div>
+                <hr/>
+
+                <div class="row">
+                    <div class="span2">
+                        <div class="thumbnail"><img src="<?=$assets_path?>img/speakers/jorge.delcasar.jpeg"
+                                                    alt="Jorge del Casar">
+                            <ul class="unstyled inline">
+                                <li><a href="https://twitter.com/#!/jorgecasar" class="sb small twitter">Twitter</a></li>
+                            </ul>
+						</div>
+                    </div>
+                    <div class="span10">
+                        <h3>Workshop: Blackberry SDK</h3>
+                        <p>Por <strong>Jorge del Casar</strong></p>
+                        <h4>BIO</h4>
+                        <p>Tras 10 años en el desarrollo web, trabajando en empresas como Softonic.com,
+                            me he cambiado de bando al desarrollo de aplicaciones
+                            móviles sin dejar de lado las tecnologías web,
+                            sigo desarrollando usando los standards HTML5 y por supuesto Javascript.
+                            Actualmente soy BlackBerry Developer Evangelist, por lo que enseño
+                            y promuevo el desarrollo en la nueva plataforma BlackBerry 10.</p>
+                        <h4>Sobre la charla</h4>
+                        <p>BlackBerry 10 dispone del navegador con mejor soporte a HTML5
+                            del mercado actual. Y por ello damos la oportunidad de desarrollar
+                            aplicaciones utilizando tus conocimientos de HTML, CSS y Javascript
+                            que correrán como aplicación local. Veremos cómo emplear dichos
+                            conocimientos en el desarrollo de aplicaciones móviles y algunas
+                            de las APIs que pone WebWorks a tu disposición para acceder a
+                            información específica de BlackBerry.</p>
+                        <h4 class="label label-important">Requerimientos</h4>
+                        <ul>
+                            <li><a href="https://developer.blackberry.com/html5/download/">BlackBerry 10 WebWorks SDK</a></li>
+                            <li><a href="<https://developer.blackberry.com/html5/download/">BlackBerry 10 Simulator</a>:
+                             Requiere <a href="http://www.vmware.com/products/player/">Vmware Player for Windows</a> (free) or
+                             <a href="http://www.vmware.com/products/fusion/overview.html">VMware Fusion for Mac</a>
+                             (43,07€ - oferta Mayo: 34,45€). En caso de ser un número reducido de asistentes se suministrará
+                             un Dev Alpha B para el despliegue de la aplicación.</li>
+                             <li>Navegador <a href="https://chrome.google.com">Google Chrome</a></li>
+                             <li><a href="https://chrome.google.com/webstore/detail/ripple-emulator-beta/geelfhphabnejjhdalkjhgipohgpdnoc">Plugin Ripple Emulator</a> (<a href="http://ripple.incubator.apache.org/">Apache Ripple Open Source Project</a>)
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="span2">
+                        <div class="thumbnail">
+                        	<img src="<?=$assets_path?>img/speakers/tomas.corral.jpg" alt="Tomás Corral">
+                            <ul class="unstyled inline">
+                                <li><a href="https://twitter.com/#!/amischol" class="sb small twitter">Twitter</a></li>
+                            </ul>
+						</div>
+                    </div>
+                    <div class="span10">
+                        <h3>Workshop: Hydra.js con las manos en la masa</h3>
+                        <p>Por <strong>Tomás Corral</strong></p>
+                        <h4>BIO</h4>
+                        <p> Javascript Arquitect Engineer en Softonic. Coordinador de CoderDojo Barcelona
+                            y otros eventos que permitan divulgar la programación como una forma
+                            de vida y diversión, soy un apasionado por las buenas prácticas y mejor
+                            código, convencido que esa es la forma de trabajar si quieres que el futuro
+                            no te alcance. El afán de divulgación del conocimiento me ha llevado
+                            a crear y colaborar en muchos proyectos open source como:
+                            Hydra.js, Hermes.js, Lungo.js, Quo.js, Karma runner(antes Testacular), Buster.js...
+                        </p>
+                        <h4>Sobre la charla</h4>
+                        <p><a href="http://tcorral.github.io/Hydra.js/" title="tcorral.github.io: Hydra.js">Hydra.js</a>
+                        	is an easy-to-use JavaScript framework that provides you with the necessary
+                        	tools to create your application using modules and widgets. It is actively used
+                        	in high traffic websites, as for example softonic.com (nearly 1 billion PV per month).
+                        	During the workshop we are going to build a sample application that will you
+                        	familiarize in no-time with all features that Hydra provides.
+                        </p>
+                    </div>
+                </div>
+                <hr/>
+
+            </div>
+			<div class="tab-pane" id="horario">
+				<h2>Horario</h2>
+				<table class="table table-bordered">
+					<tbody>
+					<tr>
+						<th><i class="icon-time"></i> 10:30 - 11:00</th>
+						<td>APERTURA - RECEPCI&Oacute;N</td>
+					</tr>
+					<tr>
+						<th><i class="icon-time"></i> 11:00 - 12:00</th>
+						<td>Charla: SEO for Single Page Webapps / Websites</td>
+					</tr>
+					<tr>
+						<th><i class="icon-time"></i> 12:00 - 12:30</th>
+						<td><i>Networking</i></td>
+					</tr>
+					<tr>
+						<th><i class="icon-time"></i> 12:30 - 13:30</th>
+						<td>Charla: Seguridad en JS y especialmente en Node.js</td>
+					</tr>
+					<tr>
+						<th><i class="icon-time"></i> 13:30 - 15:00</th>
+						<td><i>Comida (Pizzas cortes&iacute;a de Softonic!)</i></td>
+					</tr>
+					<tr>
+						<th rowspan="2"><i class="icon-time"></i> 15:00 - 18:00 <i class="icon-exclamation-sign"></i></th>
+						<td>Workshop: Hydra.js con las manos en la masa</td>
+					</tr>
+					<tr>
+						<td>Workshop: Blackberry SDK</td>
+					</tr>
+					</tbody>
+				</table>
+				<div class="alert alert-info"><i class="icon-exclamation-sign"></i> Importante: Los workshops son en
+					paralelo</div>
+			</div>
+		</div>
+
+		<script>
+			$('.nav-tabs a').click(function (e) {
+				e.preventDefault();
+				$(this).tab('show');
+			});
+			$(function () {
+				$('#eventContents a:first').tab('show');
+			});
+		</script>
+	</div>
+</div>
