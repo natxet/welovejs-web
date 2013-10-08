5c59ffd72f2611f3cfd8c14577f870e96c33bd94
unknown <thomas.klein@ST-L213-THOMKLE.domino.softonic.com>
2013-10-08 13:42:26 +0200

    Added new Welovejs event for 26.10.

---
+++ templates/welovejs6-2013-fall.php
@@ -0,0 +1,158 @@
+<div class="page-header">
+	<h1>Pr&oacute;ximo evento: 26 de Octubre de 2013
+		<a class="btn btn-large btn-success" href="http://welovejs6.eventbrite.com/"
+		   target="_blank">¡Consigue tu entrada!</a>
+		</h1>
+</div>
+<div class="row">
+	<div class="span12">
+		<nav>
+			<ul class="nav nav-tabs" id="eventContents">
+				<li><a href="#talks">Charlas</a></li>
+				<li><a href="#horario">Horario</a></li>
+			</ul>
+		</nav>
+
+		<div class="tab-content">
+			<div class="tab-pane" id="talks">
+				<h2>Charlas</h2>
+				<div class="row">
+					<div class="span2">
+						<div class="thumbnail">
+							<img src="<?=$assets_path?>img/speakers/raul.jimenez.png" alt="Raul Jimenez" />
+						</div>
+					</div>
+					<div class="span10">
+						<h3>Charla: Problemas encontrado sobre la marcha haciendo el proyecto Videogular con AngularJS</h3>
+						<p>Por <strong>Raul Jimenez</a></p>
+						<h4>Biografía</h4>
+						<p>Desarrollador con más de 10 años en web, multimedia y móviles.
+							Ha desarrollado soluciones de vídeo para Telecinco, RTVE,
+							Cuatro TV, Vocento y Grupo Z entre otros. Actualmente trabaja
+							como desarrollador freelance en Barcelona ofreciendo consultoría
+							en ActionScript, JavaScript, vídeo streaming y desarrollo móvil.
+						</p>
+						<h4>Sobre la charla</h4>
+						<p><a href="https://github.com/2fdevs/videogular">Videogular</a> es un proyecto Open Source para desarrollar un
+							player de vídeo configurable para AngularJS. En esta charla veremos cómo está desarrollado, qué problemas
+							encontramos por el camino, consejos sobre AngularJS y cómo está pensada la API para crear tus propios plugins.
+						 </p>
+						 <h4 class="label label-important">Requerimientos: Técnicos y cognitivos</h4>
+						 <p>Conocimientos de JavaScript. Preferible tener conocimientos básicos en
+							AngularJS y otro framework del estilo (Ember o Backbone).</p>
+					</div>
+				</div>
+				<hr/>
+
+				<div class="row">
+					<div class="span2">
+						<div class="thumbnail">
+							<img src="<?=$assets_path?>img/speakers/marc.rubino.jpg" alt="Marc Rubiño"></div>
+						</div>
+					</div>
+					<div class="span10">
+						<h3>Charla: Introducción a Knockout.js y el patrón MVVM desde Javascript</h3>
+						<p>Por <strong>Marc Rubi&ntilde;o</strong></p>
+						<h4>Biografía</h4>
+						<p>Marc Rubi&ntilde;o Key Consultant en Pasiona, entusiasta de la tecnología con más de
+							10 a&ntilde;os de experiencia en aplicaciones web y consultoría técnica.
+							Fundador del grupo de usuarios .Net LonetCamp. Colaborador habitual con los grupos
+							de usuarios y la fundación Techdencias. Microsoft MVP ASP.NET-IIS desde el 2011.</p>
+						<h4>Sobre la charla</h4>
+						<p><a href="http://knockoutjs.com/">Knockout</a> es una librería JavaSript que te permite crear interfaces de usuario
+							interactivas y orientadas a datos. De una manera muy sencilla y con enlaces
+							declarativos le podrás sacar el máximo partido a tu web utilizando el patrón MVVM
+							desde JavaScript.</p>
+						<h4 class="label label-important">Requerimientos: Técnicos y cognitivos</h4>
+						<p>Conocimientos de JavaScript. Preferible tener conocimientos básicos en
+							AngularJS y otro framework del estilo (Ember o Backbone).</p>
+					</div>
+				</div>
+				<hr/>
+
+				<h2>Workshops</h2>
+				<div class="row">
+					<div class="span2">
+						<div class="thumbnail">
+							<img src="<?=$assets_path?>img/speakers/raul.jimenez.png" alt="Raul Jimenez" />
+						</div>
+					</div>
+					<div class="span10">
+						<h3>Workshop: Desarollo de un plugin para Videogular con AngularJS and Bootstrap 3</h3>
+						<p>Por <strong>Raul Jimenez</strong></p>
+						<h4>Sobre el workshop</h4>
+						<p>En este workshop desarrollaremos un plugin para <a href="https://github.com/2fdevs/videogular">Videogular</a>
+							y lo aplicaremos a una aplicación de ejemplo. Aprenderemos qué son y cómo utilizar los two-way
+							bindings, uso de la API de eventos, AngularJS y Bootstrap 3, además de un montón
+							de sugerencias sobre vídeo HTML5.</p>
+						<h4 class="label label-important">Requerimientos: Técnicos y cognitivos</h4>
+						<p>Conocimientos de JavaScript. Preferible tener conocimientos básicos en
+							AngularJS y otro framework del estilo (Ember o Backbone).</p>
+					</div>
+				</div>
+				<hr/>
+
+				<div class="row">
+					<div class="span2">
+						<div class="thumbnail">
+							<img src="<?=$assets_path?>img/speakers/marc.rubino.jpg" alt="Marc Rubiño"></div>
+						</div>
+					</div>
+					<div class="span10">
+						<h3>Workshop: Desarollo de una tienda online con Knockout y MVVM</h3>
+						<p>Por <strong>Marc Rubi&ntilde;o</strong></p>
+						<h4>Sobre el workshop</h4>
+						<p>En este workshop desarrollaremos una tienda online con un catálogo de productos,
+							donde podremos comprobar el potencial que nos aporta el enlace a datos
+							bidireccional y las plantillas html utilizando un patrón MVVM con Knockout.</p>
+						<h4 class="label label-important">Requerimientos: Técnicos y cognitivos</h4>
+						<p>Conocimientos de JavaScript. Preferible tener conocimientos básicos en
+							AngularJS y otro framework del estilo (Ember o Backbone).</p>
+					</div>
+				</div>
+
+			</div>
+			<div class="tab-pane" id="horario">
+				<h2>Horario</h2>
+				<table class="table table-bordered">
+					<tbody>
+					<tr>
+						<th><i class="icon-time"></i> 9:45 - 10:00</th>
+						<td>APERTURA - RECEPCI&Oacute;N</td>
+					</tr>
+					<tr>
+						<th><i class="icon-time"></i> 10:00 - 11:00</th>
+						<td>Charla: Problemas encontrado sobre la marcha haciendo el proyecto Videogular con AngularJS</td>
+					</tr>
+					<tr>
+						<th><i class="icon-time"></i> 11:00 - 12:00</th>
+						<td>Charla: Introducción a Knockout.js y el patrón MVVM desde Javascript</td>
+					</tr>
+					<tr>
+						<th><i class="icon-time"></i> 12:00 - 14:00</th>
+						<td><i>Comida (Pizzas cortes&iacute;a de Softonic!)</i></td>
+					</tr>
+					<tr>
+						<th><i class="icon-time"></i> 14:00 - 17:00</th>
+						<td>Workshop: Desarollo de un plugin para Videogular con AngularJS and Bootstrap 3</td>
+					</tr>
+					<tr>
+						<th><i class="icon-time"></i> 14:00 - 17:00</th>
+						<td>Workshop: Desarollo de una tienda online con Knockout y MVVM</td>
+					</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+
+		<script>
+			$('.nav-tabs a').click(function (e) {
+				e.preventDefault();
+				$(this).tab('show');
+			});
+			$(function () {
+				$('#eventContents a:first').tab('show');
+			});
+		</script>
+	</div>
+</div>
